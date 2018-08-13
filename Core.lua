5f4fa5a4dbb11c3a32e34effd0a0090026d3d818
Ivan Datsko <miturtow@gmail.com>
2018-08-13 20:53:36 +0300

    Get to work with GUID more to identify units.

--- Core.lua
+++ Core.lua
@@ -1,11 +1,10 @@
 local events = {
-    -- ["npu_added"] = "NAME_PLATE_UNIT_ADDED",
-    -- ["npu_removed"] = "NAME_PLATE_UNIT_REMOVED",
-    -- ["bg"] = "PLAYER_ENTERING_BATTLEGROUND",
-    -- ["gru"] = "GROUP_ROSTER_UPDATE",
-    ["ptc"] = "PLAYER_TARGET_CHANGED"
+    ["npu_added"] = "NAME_PLATE_UNIT_ADDED",
+    ["npu_removed"] = "NAME_PLATE_UNIT_REMOVED",
+    ["bg"] = "PLAYER_ENTERING_BATTLEGROUND",
+    ["gru"] = "GROUP_ROSTER_UPDATE",
+    ["ptc"] = "PLAYER_TARGET_CHANGED",
 }
-local sdb = CreateFrame("Frame")
 local icons = {
     ["friend"] = 645193,
     ["DEMONHUNTER"] = 236415,
@@ -19,38 +18,28 @@ local icons = {
     ["SHAMAN"] = 626006,
     ["WARLOCK"] = 626007,
     ["WARRIOR"] = 626008,
-    ["DEATHKNIGHT"] = 135771
+    ["DEATHKNIGHT"] = 135771,
 }
-local classes = {
-    ["WARRIOR"] = 1,
-    ["PALADIN"] = 2,
-    ["HUNTER"] = 3,
-    ["ROGUE"] = 4,
-    ["PRIEST"] = 5,
-    ["DEATHKNIGHT"] = 6,
-    ["SHAMAN"] = 7,
-    ["MAGE"] = 8,
-    ["WARLOCK"] = 9,
-    ["MONK"] = 10,
-    ["DRUID"] = 11,
-    ["DEMONHUNTER"] = 12
-}
-local partyMembers = {}
-local GUIDSet = {}
+local guidSet = {}
+
+local sdb = CreateFrame("Frame")
+
+local _G = _G
 local GetNamePlateForUnit = _G.C_NamePlate.GetNamePlateForUnit --get nameplate from global UI variable _G
 local iconFrame, iconTexture
 
 local function addIcon(parentFrame, icon)
-    iconFrame = CreateFrame("Frame")
-    iconTexture = iconFrame:CreateTexture(nil, "BACKGROUND")
+    iconFrame = CreateFrame("Frame", nil, parentFrame)
     iconFrame:SetFrameStrata("BACKGROUND")
     iconFrame:SetWidth(32)
     iconFrame:SetHeight(32)
-    iconFrame:SetPoint("TOPLEFT", 10, 10)
-    iconFrame:SetParent(parentFrame)
-    iconFrame.texture = iconTexture
-    iconTexture:SetAllPoints(iconFrame)
+
+    iconTexture = iconFrame:CreateTexture(nil, "BACKGROUND")
     iconTexture:SetTexture(icons[icon])
+    iconTexture:SetAllPoints(iconFrame)
+    iconFrame.texture = iconTexture
+
+    iconFrame:SetPoint("TOPLEFT", 10, 10)
     iconFrame:Show()
 end
 
@@ -58,48 +47,47 @@ local function removeIcon(parentFrame)
     iconFrame:Hide()
 end
 
-local function nameplateUnit(self, event, id)
-    if event == events["npu_added"] and not GUIDSet:contains(unitGUID) then
+
+local function nameplateUnitAdded(self, event, id)
+    if event == events["npu_added"] then
+        local namePlate = GetNamePlateForUnit(id)
+        local unit_frame = namePlate.UnitFrame
         local unitGUID = UnitGUID(id)
-        local _, classId, _, _, _, unitName = GetPlayerInfoByGUID(unitGUID)
-        GUIDSet[unitGUID] = unitName
-        print(unitName .. " - " .. id .. " - " .. classId .. " added!")
-        addIcon(GetNamePlateForUnit(id), classId)
-    elseif event == events["npu_removed"] and GUIDSet:contains(unitGUID) then
-        GUIDSet[unitGUID] = nil
-        removeIcon(GetNamePlateForUnit(id))
-    end
-end
-
-local function party(self, event)
-    if event == events["gru"] then
-        for i = 1, 4 do
-            local member = "PartyMemberFrame" .. i
-            local name = _G[member].name:GetText()
-            if name then
-                partyMembers[i] = name
-            end
-            addIcon(member, UnitClass(name))
+        local separator = ", "
+        local className, classId, raceName, raceId, gender, name, realm = GetPlayerInfoByGUID(unitGUID)
+        if classId == "WARRIOR" and not guidSet:contains(unitGUID) then
+            guidSet[unitGUID] = classId
+            print(name .. separator .. classId .. separator .. realm .. separator .. id)
+        -- else
+        --     namePlate.UnitFrame:Hide()
         end
+    -- elseif event == events["npu_removed"] then
+    --     if id == "nameplate1" then
+    --         print(unitName .. " with " .. id .. " removed!")
+    --         removeIcon(namePlate)
+    --     end
     end
+    for k,v in pairs(guidSet) do
+        addIcon()
+    end
+    
 end
 
-local function onTarget(self, event)
-    local unitGUID = UnitGUID("target")
-    local _, classId, _, _, _, unitName = GetPlayerInfoByGUID(unitGUID)
-    local nameplate = GetNamePlateForUnit("target")
-    print(unitGUID .. ", " .. classId .. ", " .. unitName)
-    addIcon(nameplate, classId)
+local function party(self, event, ...)
+    if event == events["gru"] then
+        print(event)
+        print(...)
+    end
+    
 end
 
-function GUIDSet:contains(GUID)
-    return self[GUID] ~= nil
+-- Utility functions
+
+function guidSet:contains(key)
+    return self[key] ~= nil
 end
 
-for k, v in pairs(events) do
+for k,v in pairs(events) do
     sdb:RegisterEvent(v)
 end
-
-sdb:SetScript("OnEvent", onTarget)
--- sdb:SetScript("OnEvent", party)
--- sdb:SetScript("OnEvent", nameplateUnit)
+sdb:SetScript("OnEvent", nameplateUnitAdded)
